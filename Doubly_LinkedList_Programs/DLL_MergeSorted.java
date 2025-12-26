13ae37f085dd857fb46f9e79a66dcd7e0c6d7b74
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-26 08:44:55 +0530

    12

--- Doubly_LinkedList_Programs/DLL_MergeSorted.java
+++ Doubly_LinkedList_Programs/DLL_MergeSorted.java
@@ -1,18 +1,18 @@
 class Solution {
-    public DoublyNode merge(DoublyNode h1,DoublyNode h2){
-        if(h1==null)return h2;
-        if(h2==null)return h1;
+    public DoublyNode merge(DoublyNode head1,DoublyNode head2){
+        if(head1==null)return head2;
+        if(head2==null)return head1;
 
-        if(h1.data<=h2.data){
-            h1.next=merge(h1.next,h2);
-            if(h1.next!=null)h1.next.prev=h1;
-            h1.prev=null;
-            return h1;
+        if(head1.data<=head2.data){
+            head1.next=merge(head1.next,head2);
+            if(head1.next!=null)head1.next.prev=head1;
+            head1.prev=null;
+            return head1;
         }else{
-            h2.next=merge(h1,h2.next);
-            if(h2.next!=null)h2.next.prev=h2;
-            h2.prev=null;
-            return h2;
+            head2.next=merge(head1,head2.next);
+            if(head2.next!=null)head2.next.prev=head2;
+            head2.prev=null;
+            return head2;
         }
     }
 }
